6e4d8524f83d72961b06ffdaa7fa7aefa05e0e2b
JTKeil <johntkeil@gmail.com>
2017-01-17 22:35:24 -0500

    First commit

---
+++ CtestUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 
+
+namespace CtestUnitTest
+{
+    [TestClass]
+    public class httrst
+    {
+        [TestMethod]
+        public void TestListen()
+        {
+            Listener 
+        }
+    }
+
+
+}
